31062939c7e90a7975e0a284ae6b47304ea5d6e9
Aults <aultssteam@gmail.com>
2021-02-23 01:27:30 -0500

    Additional adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ function factorial(k) {
 }
 
 function binomial() {
-    var fn = document.getElementById("fn2").value;
+    var fn = parseInt(document.getElementById("fn2").value);
     var fp = document.getElementById("fp2").value;
     var p = new Array();
     var text = [];
@@ -17,15 +17,17 @@ function binomial() {
     text.push("p0 - " + p[0] + "<br>");
     for(var y = 1; y <= fn; y++) {
         p[y] = (fp/(1-fp))*((fn-(y-1))/(y))*p[y-1];
-        text.push("p[z] when P is " + fp + " and n is " + fn + " = " + p[y] + "<br>");
+        text.push("p[Y = " + y + "] = " + p[y] + "<br>");
     }
     var len = p.length-1
     text.push("Final answer, when p[" + len + "] is " + p[len] + "<br>")
-    document.getElementById("para1").innerHTML = text;
+    var str = text + "";
+    str = str.replace(/,/g, "")
+    document.getElementById("para1").innerHTML = str;
 }
 
 function poisson() {
-    var fn = document.getElementById("fz1").value;
+    var fn = parseInt(document.getElementById("fz1").value);
     var lambda = document.getElementById("lambda").value;
     var p = new Array();
     var text = [];
@@ -33,9 +35,11 @@ function poisson() {
     text.push("p0 - " + p[0] + "<br>");
     for(var z = 1; z <= fn; z++) {
         p[z] = (lambda)/(z)*p[z-1];
-        text.push("p[z] when lambda is " + lambda + " and z is " + z + " = " + p[z] + "<br>");
+        text.push("p[Z = " + z + "] = " + p[z] + "<br>");
     } 
     var len = p.length-1
-    text.push("Final answer, when p[" + len + "] is " + p[len] + "<br>")
-    document.getElementById("para").innerHTML = text;
+    text.push("Final answer, when p[" + len + "] = " + p[len] + "<br>")
+    var str = text + "";
+    str = str.replace(/,/g, "")
+    document.getElementById("para").innerHTML = str;
 }
